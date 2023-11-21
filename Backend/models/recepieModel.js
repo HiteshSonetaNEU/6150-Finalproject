0675f58ac287274a4b461dda3ee22b58670ee28d
vrishab-shetty <shetty.vr@northeastern.edu>
2023-11-21 13:18:12 -0500

    Changes in Recepie Model

--- Backend/models/recepieModel.js
+++ Backend/models/recepieModel.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
-
-const refString = "User";
+const {refString: userRef} = require("./userModel");
+const {refString: commentRef} = require("./commentModel");
+const refString = "Recepie";
 
 const recepieSchema = new mongoose.Schema({
   ingredients: [String],
   description: String,
   title: String,
   photo: String,
-  chefID: { type: mongoose.Schema.Types.ObjectId, ref: refString },
-  comments:[mongoose.Schema.Types.ObjectId]
+  chefID: { type: mongoose.Schema.Types.ObjectId, ref: userRef },
+  comments:[{ type: mongoose.Schema.Types.ObjectId, ref: commentRef}]
 });
 
 const Recepie = mongoose.model("Recepie", recepieSchema);
 module.exports = Recepie;
+module.exports = refString;
 
