72f0aafedfc21adb3559831bc7191521c2e8264c
Hitesh <soneta.h@northeastern.edu>
2023-11-21 10:34:02 -0500

    checks for login

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,6 +15,7 @@ const User = mongoose.model('User', {
   fullName: String,
   email: String,
   password: String,
+  role:String
 });
 
 const initializePassport = require('./passport-config');
@@ -24,7 +25,6 @@ initializePassport(
   id => User.findById(id)
 );
 
-app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(bodyParser.json());
@@ -38,15 +38,51 @@ app.use(passport.session());
 app.use(methodOverride('_method'));
 
 app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.fullName });
+  res.json({ name: req.user.fullName });
 });
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local'));
+// app.post('/login', checkNotAuthenticated, passport.authenticate('local'));
+
+app.post('/login', checkNotAuthenticated, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ message: 'Internal Server Error' });
+      }
+      return res.status(200).json({ message: 'Logged in successfully', user: req.user.fullName });
+    });
+  })(req, res, next);
+});
+
+
 
 app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = new User({ fullName: req.body.name, email: req.body.email, password: hashedPassword });
+    const { fullName, email, password,role } = req.body;
+      
+        if (!checkFullName(fullName)){
+          return res.status(400).json({ message: 'Special Characters in full name are not allowed' });
+        }
+      
+        if (!checkEmail(email) ) {
+          return res.status(400).json({ message: 'Invalid email, use Northeastern Mail' });
+        }
+      
+        if (!checkPassword(password)) {
+          return res.status(400).json({ message: 'Invalid password, password length should be greater than 8' });
+        }
+        var check = await User.findOne({ email });
+        if (check){
+          return res.status(400).json({ message: 'User with same email already exists' });
+        }    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ fullName, email, password: hashedPassword,role });
     await user.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (err) {
@@ -75,6 +111,17 @@ function checkNotAuthenticated(req, res, next) {
     return res.status(400).json({ message: 'Logout first' });
   }
   next();
+}
+
+function checkPassword(password) {
+  return password.length >= 8;
+}
+
+function checkEmail(email) {
+  return /^[a-zA-Z0-9._-]+@northeastern\.edu$/.test(email);
+}
+function checkFullName(fullName){
+  return  /^[a-zA-Z_]+$/.test(fullName);
 }
 
 app.listen(3000, () => {
