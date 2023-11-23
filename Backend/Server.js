9653b5297ab2949dbf1334aa45f5f7b2448c8910
OmkumarSolanki <144753825+OmkumarSolanki@users.noreply.github.com>
2023-11-23 16:03:21 -0500

    Login Page Backend Connection

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,40 +1,50 @@
-const express = require('express');
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const User =require('./models/userModel')
-const app = express();
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const User = require("./models/userModel");
+const cors = require("cors");
 
-const uri = "mongodb+srv://sonetah:Welcome123@cluster0.uv0cee1.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const uri =
+  "mongodb+srv://sonetah:Welcome123@cluster0.uv0cee1.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const initializePassport = require('./passport-config');
+const initializePassport = require("./passport-config");
 initializePassport(
   passport,
-  email => User.findOne({ email }),
-  id => User.findById(id)
+  (email) => User.findOne({ email }),
+  (id) => User.findById(id)
 );
 
+const corsOptions = {
+  origin: "http://localhost:3000", // replace with the actual origin of your frontend
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(bodyParser.json());
-app.use(session({
-  secret: "Secret",
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "Secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
-app.use('/', routes);
+app.use("/", routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ Frontend/my-app/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home Page!</h2>
+    </div>
+  );
+}
+
+export default Home;
