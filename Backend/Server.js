2f6618cdddfc3df6cf9b991c33eb37c52c617862
“vishveshpanchal” <panchal.vis@northeastern.edu>
2023-11-23 17:58:24 -0500

    Reverted server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -6,9 +6,13 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const { User } = require("./models/userModel");
 const app = express();
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors");
+const commentRoutes = require("./commentRoutes");
+const feedbackRoutes = require("./feedbackRoutes");
+const recepieRoutes = require('./routes/recepieRoutes');
+
 
 const uri =
   "mongodb+srv://sonetah:Welcome123@cluster0.uv0cee1.mongodb.net/?retryWrites=true&w=majority";
@@ -21,12 +25,6 @@ initializePassport(
   (id) => User.findById(id)
 );
 
-const corsOptions = {
-  origin: "http://localhost:3000", // replace with the actual origin of your frontend
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(bodyParser.json());
@@ -42,6 +40,9 @@ app.use(passport.session());
 app.use(methodOverride("_method"));
 
 app.use("/", routes);
+app.use("/comment", commentRoutes);
+app.use("/feedback", feedbackRoutes);
+app.use('/recepie', recepieRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
