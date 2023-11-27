3839f1e0d42e08da8f4f11323c5b1869ff01a224
OmkumarSolanki <144753825+OmkumarSolanki@users.noreply.github.com>
2023-11-27 17:09:58 -0500

    Added Basic Structure

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -9,16 +9,22 @@ import {
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import Home from "./Components/Home";
+import Chefs from "./Components/Chefs";
+import Contact from "./Components/Contact";
+import Feedback from "./Components/Feedback";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/chefs" element={<Chefs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Routes>
     </Router>
   );
