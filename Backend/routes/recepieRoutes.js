840d358ac342ad270200bb27b35a8fa0f2d2e891
OmkumarSolanki <144753825+OmkumarSolanki@users.noreply.github.com>
2023-11-30 01:17:30 -0500

    Modal Changes

--- Backend/routes/recepieRoutes.js
+++ Backend/routes/recepieRoutes.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const recepieController = require('../controllers/recepieController');
-const {checkAuthenticated}= require('../controllers/userController')
+const recepieController = require("../controllers/recepieController");
+const { checkAuthenticated } = require("../controllers/userController");
 
 // Create a new recipe
-router.post('/create', checkAuthenticated,recepieController.createRecepie);
+router.post("/create", checkAuthenticated, recepieController.createRecepie);
 
 // Get all recipes
-router.get('/get',checkAuthenticated, recepieController.getAllRecepies);
+router.get("/get", checkAuthenticated, recepieController.getAllRecepies); // give all my recepies and the chef's recepies which I follow
 
 // Get a recipe by ID
-router.get('/:id',checkAuthenticated, recepieController.getRecepieById);
+router.get("/:id", checkAuthenticated, recepieController.getRecepieById);
 
 // Update a recipe by ID
-router.put('/:id', checkAuthenticated,recepieController.updateRecepieById);
+router.put("/:id", checkAuthenticated, recepieController.updateRecepieById);
 
 // Delete a recipe by ID
-router.delete('/:id', checkAuthenticated,recepieController.deleteRecepieById);
+router.delete("/:id", checkAuthenticated, recepieController.deleteRecepieById);
 
-router.get('/getChef/:id', checkAuthenticated, recepieController.getChefById)
+router.get("/getChef/:id", checkAuthenticated, recepieController.getChefById); // Recepie ID --> give Chef Details
 
-router.get('/chef/:id',checkAuthenticated,recepieController.getRecepieByChef)
+router.get("/chef/:id", checkAuthenticated, recepieController.getRecepieByChef); // Chef ID --> Give all recepies created by chef (chef ID)
 
 module.exports = router;
